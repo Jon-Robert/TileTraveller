27d4252ad5b7a6c10c309aace908b62ef707089f
Jón Róbert Árnason <jon.robert96@gmail.com>
2020-09-17 10:01:23 +0000

    project started

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,3 +1,16 @@
 # Assignment 8: TileTraveller
 # jonra20, rutt20, valurg20
 
+# print('You can travel: ')
+
+def movement
+
+
+xHnit = 1
+yHnit = 1
+
+
+while xHnit != 3 and yHnit != 1:
+    #do the function stuffs
+
+
